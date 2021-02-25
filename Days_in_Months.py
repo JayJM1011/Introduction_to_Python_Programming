c86c095869ca1eefc78cf3adb8236a672582bd0d
Jay Malanker <jjmalankar@gmail.com>
2021-02-25 13:18:29 +0530

    Create Days_in_Months.py

---
+++ Days_in_Months.py
@@ -0,0 +1,10 @@
+month= input('Enter Month: ')
+year= int(input('Enter Year: '))
+if month == 'January' or month == 'March' or month == 'May' or month == 'July' or month == 'August' or month == 'October' or month == 'December':
+    print(31)
+if month == 'April' or month == 'June' or month == 'September' or month == 'November':
+    print(30)
+if month == 'February' and int(year% 4) != 0:
+    print(28)
+if month == 'February' and int(year% 4) == 0:
+    print(29)
