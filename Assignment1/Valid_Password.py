410630166e8c839175909bbb102d952d948dd2ca
Jay Malanker <jjmalankar@gmail.com>
2021-03-13 01:39:07 +0530

    Create Valid_Password.py

---
+++ Assignment1/Valid_Password.py
@@ -0,0 +1,18 @@
+#import re
+#def ValidPass(Pass):
+#    if len(Pass)> 16 and len(Pass)< 6:
+#        return False
+#    elif not re.search("[a-z]", Pass): 
+#        return False
+#    elif not re.search("[A-Z]", Pass): 
+#        return False
+#    elif not re.search("[0-9]", Pass): 
+#        return False
+#    elif not re.search("[_@$]", Pass): 
+#        return False
+#    return True
+#Pass= input("Enter Password: ")
+#if ValidPass(Pass):
+#    print("Valid Password")
+#else:
+#    print("Invalid Password")
