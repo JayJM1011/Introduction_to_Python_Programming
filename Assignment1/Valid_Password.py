7c779df010e5e30a30179071e42c8ac7c6de29e0
Jay Malanker <jjmalankar@gmail.com>
2021-03-13 01:39:54 +0530

    Update Valid_Password.py

--- Assignment1/Valid_Password.py
+++ Assignment1/Valid_Password.py
@@ -7,12 +7,12 @@ def ValidPass(Pass):
     elif not re.search("[A-Z]", Pass): 
         return False
     elif not re.search("[0-9]", Pass): 
-#        return False
-#    elif not re.search("[_@$]", Pass): 
-#        return False
-#    return True
-#Pass= input("Enter Password: ")
-#if ValidPass(Pass):
-#    print("Valid Password")
-#else:
-#    print("Invalid Password")
+        return False
+    elif not re.search("[_@$]", Pass): 
+        return False
+    return True
+Pass= input("Enter Password: ")
+if ValidPass(Pass):
+    print("Valid Password")
+else:
+    print("Invalid Password")
