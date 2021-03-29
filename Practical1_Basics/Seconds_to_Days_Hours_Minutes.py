64db35e3984bae28ae5f8f4af2609e5886900801
Jay Malanker <jjmalankar@gmail.com>
2021-03-29 22:28:00 +0530

    Create Seconds_to_Days_Hours_Minutes.py

---
+++ Practical1_Basics/Seconds_to_Days_Hours_Minutes.py
@@ -0,0 +1,13 @@
+#Task7
+
+seconds= 90400
+days, hours, minutes= 0, 0, 0
+if seconds >= 86400:
+    days= int(seconds/ 86400.0)
+    seconds= seconds% 86400
+if seconds >= 3600:
+    hours= int(seconds/ 3600)
+    seconds= seconds% 3600
+if seconds < 3600:
+    minutes= seconds/ 60
+print('The given seconds is equal to {0} Days, {1} Hours, and {2} Minutes'.format(days, hours, minutes))
