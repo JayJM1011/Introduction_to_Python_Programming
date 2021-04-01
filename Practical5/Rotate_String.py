8657076e999370ca3aad4687627f0ea17be25298
Jay Malanker <jjmalankar@gmail.com>
2021-04-01 19:14:39 +0530

    Delete Rotate_String.py

--- Practical5/Rotate_String.py
+++
@@ -1,9 +0,0 @@
-def LeftRot(String, x):
-    Left= String[x: ]+ String[0: x]
-    return Left
-def RightRot(String, x):
-    Right= String[-(x): ]+ String[0: -(x)]
-    return Right
-String, x= input("Enter String: "), int(input("Enter x: "))
-print("Left Rotation:", LeftRot(String, x))
-print("Right Rotation:", RightRot(String, x))
