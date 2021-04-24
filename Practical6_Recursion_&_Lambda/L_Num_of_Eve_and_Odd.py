589e27d0f6bb6dc8666edeeba8c4628a974ace4d
Jay Malanker <jjmalankar@gmail.com>
2021-04-25 00:34:19 +0530

    Delete L_Num_of_Eve_and_Odd.py

--- Practical6_Recursion_&_Lambda/L_Num_of_Eve_and_Odd.py
+++
@@ -1,5 +0,0 @@
-List= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-Eve= filter(lambda x: x% 2 == 0, List)
-Odd= filter(lambda x: x% 2 == 1, List)
-print("Number of Even Nums:", len(list(Eve)))
-print("Number of Odd Nums:", len(list(Odd)))
